6ba48d27eb2047ae929c86b7484051653ca86937
Davide Lombroni <davide.lombroni98@gmail.com>
2024-06-18 00:02:00 +0200

    es1

---
+++ Settimana 1/Giorno 1/Es1.2/Persona.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Es1._2
+{
+    internal class Persona
+    {
+        public int età;
+        public string nome;
+    }
+}
---
+++ Settimana 1/Giorno 1/Es1.2/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Es1._2 // contenitore logico
+{
+    internal class Program //unità minima di codice che rappresenta un contenitore di più metodi ed informazioni
+    {
+        static void Main(string[] args) //Metodo: principale contenitore di codice , il metodo Main rappresenta l'entry pint dell'applicazione
+        {
+            Console.WriteLine("Hello, World!"); 
+            int x = 0;
+            Console.WriteLine(x);
+            string y = "questa è una  stringa";
+            Console.WriteLine(y);
+
+            Persona persona = new Persona();
+            persona.età = 10;
+            persona.nome = "Giorgio";
+
+            Console.WriteLine(persona.età);
+        }
+    }
+}
+
+// 
