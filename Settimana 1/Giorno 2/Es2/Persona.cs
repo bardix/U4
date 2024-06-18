3b2854ec885a40113a4b98deb5bb3012648c8f5b
Davide Lombroni <davide.lombroni98@gmail.com>
2024-06-18 14:31:01 +0200

    Es1

---
+++ Settimana 1/Giorno 2/Es2/Persona.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Es2
+{
+    internal class Persona
+    {
+        public string Nome;
+        public string Cognome;
+        public int Eta;
+
+        public Persona(string nome, string cognome, int eta)
+        {
+            Nome = nome;
+            Cognome = cognome;
+            Eta = eta;
+        }
+
+        public string GetNome()
+        {
+            return Nome;
+        }
+
+        public string GetCognome()
+        {
+            return Cognome;
+        }
+
+        public int GetEta()
+        {
+            return Eta;
+        }
+
+        public string GetDettagli()
+        {
+            return $"Nome: {Nome}, Cognome: {Cognome}, Età: {Eta}";
+        }
+
+
+    }
+}
---
+++ Settimana 1/Giorno 2/Es2/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Es2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona persona = new Persona("Mario", "Rossi", 30);
+
+            Console.WriteLine("Nome: " + persona.GetNome());
+            Console.WriteLine("Cognome: " + persona.GetCognome());
+            Console.WriteLine("Età: " + persona.GetEta());
+            Console.WriteLine("Dettagli: " + persona.GetDettagli());
+        }
+    }
+}
