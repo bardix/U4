1ebf73493e8d5499a5860bf7b0a6326121896550
Davide Lombroni <davide.lombroni98@gmail.com>
2024-06-24 15:53:34 +0200

    fine es

--- Settimana 2/Giorno 1/WebApplication1/Controllers/HomeController.cs
+++ Settimana 2/Giorno 1/WebApplication1/Controllers/HomeController.cs
@@ -15,7 +15,16 @@ namespace WebApplication1.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            var list = new List<Menu>{
+                new Menu { NameProduct="CocaCola 150", Price="€ 2.50"},
+                new Menu { NameProduct="Insalata di pollo", Price="€ 5.20"},
+                new Menu { NameProduct="Pizza Margherita", Price="€ 10.00"},
+                new Menu { NameProduct="Pizza 4 Formaggi ", Price="€12.50"},
+                new Menu { NameProduct="Pz patatine fritte", Price="€ 3.50"},
+                new Menu { NameProduct="Insalata di riso", Price="€ 8.00"},
+                new Menu { NameProduct="Frutta di stagione", Price="€ 5.00"},
+            };
+            return View(list);
         }
 
         public IActionResult Privacy()
---
+++ Settimana 2/Giorno 1/WebApplication1/Models/Menu.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication1.Models
+{
+    public class Menu
+    {
+        public required string NameProduct { get; set; }
+        public  required string Price { get; set; }
+    }
+}
